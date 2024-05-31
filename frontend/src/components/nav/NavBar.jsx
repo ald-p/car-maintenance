9a8a187b8729a12f37b39f9704e5a32153ba2ce4
Aldwin <aldwin.pag@gmail.com>
2024-05-31 14:18:46 -0400

    add navbar

---
+++ frontend/src/components/nav/NavBar.jsx
@@ -0,0 +1,101 @@
+import { IoCarSport } from 'react-icons/io5';
+
+const NavBar = () => {
+  return (
+    <div className="navbar bg-base-100 md:px-10 lg:px-10 bg-neutral text-neutral-content">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <a>Home</a>
+            </li>
+            <li>
+              <a>Dashboard</a>
+            </li>
+            <li>
+              <a>Records</a>
+            </li>
+          </ul>
+        </div>
+        <a className="btn btn-ghost text-xl">
+          <IoCarSport className="text-2xl" />
+          AutoLog
+        </a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-2 bg-base-200 rounded-box">
+          <li>
+            <a>Home</a>
+          </li>
+          <li>
+            <a>Dashboard</a>
+          </li>
+          <li>
+            <a>Records</a>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-end">
+        <div className="form-control">
+          <input
+            type="text"
+            placeholder="Search"
+            className="input input-bordered w-24 md:w-auto mr-2"
+          />
+        </div>
+        <div className="dropdown dropdown-end">
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn btn-ghost btn-circle avatar"
+          >
+            <div className="w-10 rounded-full">
+              <img
+                alt="Tailwind CSS Navbar component"
+                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+              />
+            </div>
+          </div>
+          <ul
+            tabIndex={0}
+            className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <a className="justify-between">
+                Profile
+                <span className="badge">New</span>
+              </a>
+            </li>
+            <li>
+              <a>Settings</a>
+            </li>
+            <li>
+              <a>Logout</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
