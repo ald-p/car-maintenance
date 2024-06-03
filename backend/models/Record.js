a52505747a159f063d2297c6884617f718b1b5be
Aldwin <aldwin.pag@gmail.com>
2024-06-03 15:22:06 -0400

    update records schema

--- backend/models/Record.js
+++ backend/models/Record.js
@@ -3,14 +3,24 @@ const mongoose = require('mongoose');
 const TaskSchema = new mongoose.Schema({
   type: { type: String, required: true },
   description: { type: String },
+  partsUsed: { type: [String] },
   cost: { type: Number },
 });
 
-const RecordSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  date: { type: Date, required: true },
-  tasks: [TaskSchema],
-});
+const RecordSchema = new mongoose.Schema(
+  {
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    date: { type: Date, required: true },
+    mileage: {
+      type: Number,
+      required: true,
+    },
+    tasks: [TaskSchema],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const Record = mongoose.model('Record', RecordSchema);
 
